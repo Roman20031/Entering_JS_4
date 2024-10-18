b0efb4d7b2d758a828f577f3d4c5f92198073c3a
Roman Demianenko <Roman.Demianenko@student.souepl.cz>
2024-10-18 11:36:39 +0200

    Entering_JS_4

---
+++ script.js
@@ -0,0 +1,14 @@
+
+let element = document.createElement("elementName");
+
+function Add () {
+    
+    const img = document.createElement("img");
+    /*https://picsum.photos/200/301?id=*/ 
+    /*https://preview.redd.it/a-theory-about-the-crashed-uav-v0-1z2hqyqeg5ua1.png?width=1358&format=png&auto=webp&s=2c949e90512c03eff7530d542b9751e58111fa26*/ 
+
+    /*img.src = "https://picsum.photos/200/301?";*/ 
+    img.src = "https://preview.redd.it/a-theory-about-the-crashed-uav-v0-1z2hqyqeg5ua1.png?width=1358&format=png&auto=webp&s=2c949e90512c03eff7530d542b9751e58111fa26";
+    img.classList.add("img-margin");
+    document.body.appendChild(img);
+}
